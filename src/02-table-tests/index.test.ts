5d7774fb019e339920cc7a76bde65d49d70cca7c
Stanislau <sats96dergach@gmail.com>
2025-05-09 10:56:36 +0200

    init: add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -6,14 +6,14 @@ const testCases = [
   { a: 6, b: 2, action: Action.Divide, expected: 3 },
   { a: 3, b: 2, action: Action.Multiply, expected: 6 },
   { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
-  // { a: '6', b: 2, action: Action.Add, expected: null },
-  // { a: 6, b: 2, action: 'InvalidAction', expected: null },
+  { a: '6', b: 2, action: Action.Add, expected: null },
+  { a: 6, b: 2, action: 'error action', expected: null },
 ];
 
 describe('simpleCalculator', () => {
   test.each(testCases)(
     'should return the correct result for %o',
-    (testCase: { a: number; b: number; action: Action; expected: number }) => {
+    (testCase) => {
       const { a, b, action, expected } = testCase;
       const result = simpleCalculator({ a, b, action });
       expect(result).toBe(expected);
