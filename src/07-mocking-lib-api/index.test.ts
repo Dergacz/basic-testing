a83c6931f5d0432055d107f8378224aec30589e5
dergacz <sats96dergach@gmail.com>
2026-04-13 10:52:46 +0200

    test: implement unit tests for simpleCalculator and BankAccount, add snapshot tests for generateLinkedList

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,44 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  ...jest.requireActual<typeof import('lodash')>('lodash'),
+  throttle: jest.fn((fn: (...args: unknown[]) => unknown) => fn),
+}));
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
+  const mockGet = jest.fn();
+
+  beforeEach(() => {
+    mockGet.mockResolvedValue({ data: { id: 1, title: 'test' } });
+    mockedAxios.create.mockReturnValue({
+      get: mockGet,
+    } as unknown as ReturnType<typeof axios.create>);
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/posts');
+
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/posts');
+
+    expect(mockGet).toHaveBeenCalledWith('/posts');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const responseData = { id: 1, title: 'test' };
+    mockGet.mockResolvedValue({ data: responseData });
+
+    const result = await throttledGetDataFromApi('/posts');
+
+    expect(result).toEqual(responseData);
   });
 });
